d00269e77b5f79f2248a79892b79c963a6a2935b
Carol <Cat-exe@users.noreply.github.com>
2020-10-22 12:23:16 -0400

    Found averages of lists.

--- tectonic_speeds.py
+++ tectonic_speeds.py
@@ -1,3 +1,7 @@
 # List of oceanic vs. continental plate speeds
 oceanic = [7.87, 7.74, 6.11, 7.69, 4.16, 4.0]
 continental = [1.71, 4.34, 2.00, 3.35, 1.96, 2.14]
+
+#Find the averages
+oceanic_avg = sum(oceanic)/len(oceanic)
+continental_avg = sum(continental)/len(continental)                       
