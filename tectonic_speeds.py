73391787f93125674e701b6a1bed4b73937edc96
Yvonne De La Pena <yvonnedelapena@gmail.com>
2020-10-22 13:40:51 -0400

    line plot

--- tectonic_speeds.py
+++ tectonic_speeds.py
@@ -9,3 +9,6 @@ continental = [1.71, 4.34, 2.00, 3.35, 1.96, 2.14]
 # Find the averages
 oceanic_avg = sum(oceanic)/len(oceanic)
 continental_avg = sum(continental)/len(continental)
+
+#making a line plot
+plt.plot(oceanic, continental)
