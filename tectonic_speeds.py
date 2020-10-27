df7d12dc54c05ae8c4bfd8d071fe56f302d022fc
Carol Pignato <carolpignato97@gmail.com>
2020-10-27 17:55:58 -0400

    Added message, got rid of matplotlib

--- tectonic_speeds.py
+++ tectonic_speeds.py
@@ -1,10 +1,11 @@
 
-# Import the library, matplotlib
-import matplotlib.pyplot as plt
 
 # List of oceanic vs. continental plate speeds
 oceanic = [7.87, 7.74, 6.11, 7.69, 4.16, 4.0]
 continental = [1.71, 4.34, 2.00, 3.35, 1.96, 2.14]
+
+print("Oceanic plates move faster than continental plates.")
+
 
 # Find the averages
 oceanic_avg = sum(oceanic)/len(oceanic)
